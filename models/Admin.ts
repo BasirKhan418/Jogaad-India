1e2396e580bb78cdc7c0a66af3e1661f726d9131
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-11-09 08:38:00 +0530

    models added

---
+++ models/Admin.ts
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const AdminSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    img: { type: String },
+    phone: { type: String },
+    isSuperAdmin: { type: Boolean, default: false },
+}, { timestamps: true })
+export default mongoose.models?.Admin || mongoose.model('Admin', AdminSchema);
---
+++ models/User.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true },
+    address: { type: String ,required:false},
+    phone: { type: String ,required:false},
+    isImposedFine: { type: Boolean, default: false },
+    img: { type: String,required:false},
+}, { timestamps: true })
+export default mongoose.models?.User || mongoose.model('User', UserSchema);
