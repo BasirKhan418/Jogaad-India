96015720ba0d5ce71b4c0d5c3715674be94e976a
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-11-12 12:49:36 +0530

    admin security added on creating endpoint

--- app/api/v1/(admin)/signin/route.ts
+++ app/api/v1/(admin)/signin/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse,NextRequest } from "next/server";
 import { AdminLoginSchemaZod } from "@/validator/admin/admin.auth";
 import { generateOTP } from "@/repository/admin/admin.auth";
+import { cookies } from "next/headers";
+import { AdminUpdateSchemaZod } from "@/validator/admin/admin.auth";
+import { updateAdminByEmail } from "@/repository/admin/admin.auth";
+import { verifyUserToken } from "@/utils/user/usertoken.verify";
 export const POST = async (request:NextRequest) => {
     try{
         const data = await request.json();
@@ -9,6 +13,27 @@ export const POST = async (request:NextRequest) => {
             return NextResponse.json({message:"Invalid data",success:false}, {status:400});
         }
         const response = await generateOTP(validateData.data.email);
+        return NextResponse.json(response);
+    }
+    catch(error){
+        return NextResponse.json({message:"Internal Server Error",success:false}, {status:500});
+    }
+}
+
+export const PUT = async (request:NextRequest) => {
+    try{
+        const data = await request.json();
+        const validateData = AdminUpdateSchemaZod.safeParse(data);
+        if(!validateData.success){
+            return NextResponse.json({message:"Invalid data",success:false}, {status:400});
+        }
+        const cookiesStore = await cookies();
+        const token = cookiesStore.get("token")?.value||"";
+        const isTokenValid = await verifyUserToken(token);
+        if(!isTokenValid.success||isTokenValid.type!=="admin"){
+            return NextResponse.json({message:"Invalid token",success:false}, {status:401});
+        }
+        const response = await updateAdminByEmail(isTokenValid.email, validateData.data);
         return NextResponse.json(response);
     }
     catch(error){
--- app/api/v1/(admin)/signup/route.ts
+++ app/api/v1/(admin)/signup/route.ts
@@ -1,18 +1,35 @@
-import { NextResponse,NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { AdminSchemaZod } from "@/validator/admin/admin.auth";
 import { createAdmin } from "@/repository/admin/admin.auth";
-export const POST = async (request:NextRequest) => {
-    try{
+import { cookies } from "next/headers";
+import { verifyUserToken } from "@/utils/user/usertoken.verify";
+export const POST = async (request: NextRequest) => {
+    try {
         const data = await request.json();
+        console.log("Request Data:", data);
+        const cookiesStore = await cookies();
+        const token = cookiesStore.get("token")?.value || "";
+        const isTokenValid = await verifyUserToken(token);
+        if (data.password === process.env.ADMIN_CREATE_CREDENTIAL) {
+            const validateData = AdminSchemaZod.safeParse(data);
+            if (!validateData.success) {
+                return NextResponse.json({ message: "Invalid data", success: false }, { status: 400 });
+            }
+            const response = await createAdmin(validateData.data);
+            return NextResponse.json(response);
+        }
+        if (!isTokenValid.success || isTokenValid.type !== "admin") {
+            return NextResponse.json({ message: "Invalid crdential for creating admin", success: false }, { status: 401 });
+        }
         const validateData = AdminSchemaZod.safeParse(data);
-        if(!validateData.success){
-            return NextResponse.json({message:"Invalid data",success:false}, {status:400});
+        if (!validateData.success) {
+            return NextResponse.json({ message: "Invalid data", success: false }, { status: 400 });
         }
         const response = await createAdmin(validateData.data);
         return NextResponse.json(response);
 
     }
-    catch(error){
-        return NextResponse.json({message:"Internal Server Error",success:false}, {status:500});
+    catch (error) {
+        return NextResponse.json({ message: "Internal Server Error", success: false }, { status: 500 });
     }
 }
