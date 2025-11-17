0ebb6bd7ffc80d9a20c47f20aed50e26fe7e929b
Basirkhan418 <thebasirkhanofficial@gmail.com>
2025-11-17 14:02:10 +0530

    logs added with api

--- app/api/v1/logs/bydate/route.ts
+++ app/api/v1/logs/bydate/route.ts
@@ -1,20 +1,28 @@
 import { NextResponse, NextRequest } from "next/server";
 import { fetchLogFromS3 } from "@/utils/s3Logger";
-
+import { verifyUserToken } from "@/utils/user/usertoken.verify";
+import { cookies } from "next/headers";
 export async function POST(request: NextRequest) {
-  const { date } = await request.json(); // expected format: YYYY-MM-DD
+    const { date } = await request.json(); // expected format: YYYY-MM-DD
+    const cookesStore = await cookies();
+    const userToken = cookesStore.get("token")?.value || "";
+    const isVerified = await verifyUserToken(userToken);
+    
+    if (!isVerified || isVerified.type !== "admin") {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
 
-  if (!date || !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
-    return NextResponse.json(
-      { error: "Invalid date format. Use YYYY-MM-DD." },
-      { status: 400 }
-    );
-  }
+    if (!date || !/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+        return NextResponse.json(
+            { error: "Invalid date format. Use YYYY-MM-DD." },
+            { status: 400 }
+        );
+    }
 
-  const logs = await fetchLogFromS3(date);
+    const logs = await fetchLogFromS3(date);
 
-  return NextResponse.json({
-    date,
-    logs: logs || "No logs found for this date."
-  });
+    return NextResponse.json({
+        date,
+        logs: logs || "No logs found for this date."
+    });
 }
--- app/api/v1/logs/today/route.ts
+++ app/api/v1/logs/today/route.ts
@@ -1,19 +1,27 @@
 import { NextResponse } from "next/server";
 import { fetchLogFromS3 } from "@/utils/s3Logger";
+import { verifyUserToken } from "@/utils/user/usertoken.verify";
+import { cookies } from "next/headers";
 
 export async function GET() {
-  const now = new Date();
+    const now = new Date();
+    const cookesStore = await cookies();
+    const userToken = cookesStore.get("token")?.value || "";
+    const isVerified = await verifyUserToken(userToken);
+    
+    if (!isVerified || isVerified.type !== "admin") {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    const yyyy = now.getFullYear();
+    const mm = String(now.getMonth() + 1).padStart(2, "0");
+    const dd = String(now.getDate()).padStart(2, "0");
 
-  const yyyy = now.getFullYear();
-  const mm = String(now.getMonth() + 1).padStart(2, "0");
-  const dd = String(now.getDate()).padStart(2, "0");
+    const date = `${yyyy}-${mm}-${dd}`;
 
-  const date = `${yyyy}-${mm}-${dd}`;
+    const logs = await fetchLogFromS3(date);
 
-  const logs = await fetchLogFromS3(date);
-
-  return NextResponse.json({
-    date,
-    logs: logs || "No logs found for today."
-  });
+    return NextResponse.json({
+        date,
+        logs: logs || "No logs found for today."
+    });
 }
